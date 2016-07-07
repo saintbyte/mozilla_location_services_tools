e08b9927db228030076dd1fe2839abb7d89de501
SB <admin@openwifi.su>
2016-07-07 12:50:26 +0500

    fix page parser for new page

--- get.sh
+++ get.sh
@@ -4,8 +4,9 @@ set +ue
 wget -O-  https://location.services.mozilla.com/downloads | grep "MLS-full-cell" > download.tmp 
 while read -r line;
 do
-#echo "111 ${line}"
+echo "111 ${line}"
 LASTLINE=$line
+break
 done <  download.tmp
 LAST_URL=`echo $LASTLINE | awk -F '"' '{print $2}' | awk -F '"' '{print $1}'`
 FILE=`basename $LAST_URL`
